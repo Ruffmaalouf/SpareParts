a601f7d2a4cdaa10a92222af8b4baf3035b01099
Ruffmaalouf <113841072+Ruffmaalouf@users.noreply.github.com>
2026-03-25 12:54:16 +0200

    Enlarge and polish calendar popup date selector

--- src/SpareParts.Desktop.Controls/SaleDatePickerControl.xaml.cs
+++ src/SpareParts.Desktop.Controls/SaleDatePickerControl.xaml.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -11,13 +12,10 @@ namespace SpareParts.Desktop.Wpf
                 nameof(SelectedDate),
                 typeof(DateTime?),
                 typeof(SaleDatePickerControl),
-                new FrameworkPropertyMetadata(DateTime.Today, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
-
-        public DateTime? SelectedDate
-        {
-            get => (DateTime?)GetValue(SelectedDateProperty);
-            set => SetValue(SelectedDateProperty, value);
-        }
+                new FrameworkPropertyMetadata(
+                    DateTime.Today,
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+                    OnSelectedDateChanged));
 
         public static readonly DependencyProperty LabelProperty =
             DependencyProperty.Register(
@@ -26,15 +24,43 @@ namespace SpareParts.Desktop.Wpf
                 typeof(SaleDatePickerControl),
                 new PropertyMetadata("DATE"));
 
+        public static readonly DependencyProperty SelectedDateDisplayTextProperty =
+            DependencyProperty.Register(
+                nameof(SelectedDateDisplayText),
+                typeof(string),
+                typeof(SaleDatePickerControl),
+                new PropertyMetadata("Select date"));
+
+        public DateTime? SelectedDate
+        {
+            get => (DateTime?)GetValue(SelectedDateProperty);
+            set => SetValue(SelectedDateProperty, value);
+        }
+
         public string Label
         {
             get => (string)GetValue(LabelProperty);
             set => SetValue(LabelProperty, value);
         }
 
+        public string SelectedDateDisplayText
+        {
+            get => (string)GetValue(SelectedDateDisplayTextProperty);
+            set => SetValue(SelectedDateDisplayTextProperty, value);
+        }
+
         public SaleDatePickerControl()
         {
             InitializeComponent();
+            UpdateSelectedDateDisplayText();
+        }
+
+        private static void OnSelectedDateChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is SaleDatePickerControl control)
+            {
+                control.UpdateSelectedDateDisplayText();
+            }
         }
 
         private void TogglePopup_Click(object sender, RoutedEventArgs e)
@@ -44,10 +70,33 @@ namespace SpareParts.Desktop.Wpf
 
         private void Calendar_SelectedDatesChanged(object? sender, SelectionChangedEventArgs e)
         {
-            if (SelectedDate.HasValue)
+            if (sender is Calendar calendar)
             {
-                CalendarPopup.IsOpen = false;
+                SelectedDate = calendar.SelectedDate;
             }
+        }
+
+        private void TodayButton_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedDate = DateTime.Today;
+            PickerCalendar.DisplayDate = DateTime.Today;
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedDate = null;
+        }
+
+        private void DoneButton_Click(object sender, RoutedEventArgs e)
+        {
+            CalendarPopup.IsOpen = false;
+        }
+
+        private void UpdateSelectedDateDisplayText()
+        {
+            SelectedDateDisplayText = SelectedDate.HasValue
+                ? SelectedDate.Value.ToString("dddd, dd MMM yyyy", CultureInfo.CurrentCulture)
+                : "Select date";
         }
     }
 }
