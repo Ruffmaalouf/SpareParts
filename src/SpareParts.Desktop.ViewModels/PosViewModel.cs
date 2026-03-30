282c3ddb7d9f4f99617a25d515f31573a82463ec
Ruffmaalouf <113841072+Ruffmaalouf@users.noreply.github.com>
2026-03-30 09:50:30 +0300

    Add AR experience screen to desktop app

--- src/SpareParts.Desktop.ViewModels/PosViewModel.cs
+++ src/SpareParts.Desktop.ViewModels/PosViewModel.cs
@@ -41,7 +41,7 @@ namespace SpareParts.Desktop.Wpf
         public ObservableCollection<CarModelUi>  AvailableCars  { get; } = new();
         public ObservableCollection<CarPartModel> AvailableParts { get; } = new();
 
-        public enum AppScreen { HomePage, CarSelection, PartSelection, Pos, Purchases, StockManagement }
+        public enum AppScreen { HomePage, CarSelection, PartSelection, Pos, Purchases, StockManagement, ArExperience }
 
         private AppScreen _activeScreen = AppScreen.HomePage;
         public AppScreen ActiveScreen
