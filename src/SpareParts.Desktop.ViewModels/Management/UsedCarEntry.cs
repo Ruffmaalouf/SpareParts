e9a6aed0407c52d450a00951688a6d5f633f81cc
Ruffmaalouf <113841072+Ruffmaalouf@users.noreply.github.com>
2026-04-07 15:57:31 +0300

    Fix used car management bindings and edit behavior

--- src/SpareParts.Desktop.ViewModels/Management/UsedCarEntry.cs
+++ src/SpareParts.Desktop.ViewModels/Management/UsedCarEntry.cs
@@ -1,17 +1,110 @@
+using System.ComponentModel;
+using System.Collections.Generic;
+
 namespace SpareParts.Desktop.Wpf.Management
 {
-    public class UsedCarEntry
+    public class UsedCarEntry : INotifyPropertyChanged
     {
-        public string Car { get; set; } = string.Empty;
-        public int ModelYear { get; set; }
-        public string PriceCurrency { get; set; } = "USD";
-        public decimal PriceBase { get; set; }
-        public decimal PriceCounter { get; set; }
-        public string Location { get; set; } = string.Empty;
-        public decimal Transportation { get; set; }
+        private string _car = string.Empty;
+        private int _modelYear;
+        private string _priceCurrency = "USD";
+        private decimal _priceBase;
+        private decimal _priceCounter;
+        private string _location = string.Empty;
+        private decimal _transportation;
+        private string _partOut = string.Empty;
+        private decimal _shipping;
+        private decimal _customs;
+
+        public string Car
+        {
+            get => _car;
+            set => SetField(ref _car, value);
+        }
+
+        public int ModelYear
+        {
+            get => _modelYear;
+            set => SetField(ref _modelYear, value);
+        }
+
+        public string PriceCurrency
+        {
+            get => _priceCurrency;
+            set => SetField(ref _priceCurrency, value);
+        }
+
+        public decimal PriceBase
+        {
+            get => _priceBase;
+            set => SetField(ref _priceBase, value);
+        }
+
+        public decimal PriceCounter
+        {
+            get => _priceCounter;
+            set
+            {
+                if (SetField(ref _priceCounter, value))
+                {
+                    OnPropertyChanged(nameof(TotalBeforeShipping));
+                }
+            }
+        }
+
+        public string Location
+        {
+            get => _location;
+            set => SetField(ref _location, value);
+        }
+
+        public decimal Transportation
+        {
+            get => _transportation;
+            set
+            {
+                if (SetField(ref _transportation, value))
+                {
+                    OnPropertyChanged(nameof(TotalBeforeShipping));
+                }
+            }
+        }
+
         public decimal TotalBeforeShipping => PriceCounter + Transportation;
-        public string PartOut { get; set; } = string.Empty;
-        public decimal Shipping { get; set; }
-        public decimal Customs { get; set; }
+
+        public string PartOut
+        {
+            get => _partOut;
+            set => SetField(ref _partOut, value);
+        }
+
+        public decimal Shipping
+        {
+            get => _shipping;
+            set => SetField(ref _shipping, value);
+        }
+
+        public decimal Customs
+        {
+            get => _customs;
+            set => SetField(ref _customs, value);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private bool SetField<T>(ref T field, T value, string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+            {
+                return false;
+            }
+
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        private void OnPropertyChanged(string? propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
